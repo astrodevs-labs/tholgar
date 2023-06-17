990be090aea4fc3a0b8a97bce33979fe94c061c8
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-06-17 16:46:52 +0200

    doc: Vault.sol

--- src/Vault.sol
+++ src/Vault.sol
@@ -10,6 +10,9 @@ import {ERC4626} from "solmate/mixins/ERC4626.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {WAR} from "./utils/constants.sol";
 
+/// @author 0xtekgrinder
+/// @title Vault contract
+/// @notice This contract is the auto compounding vault for the warlord protocol
 contract Vault is ERC4626, Owner {
     using FixedPointMathLib for uint256;
 
@@ -25,36 +28,61 @@ contract Vault is ERC4626, Owner {
         ERC20(WAR).approve(initialStaker, type(uint256).max);
     }
 
-    function setSwap(address newSwap) external onlyOwner {
-        swap = newSwap;
-    }
-
-    function setStaker(address newStaker) external onlyOwner {
-        uint256 stakerBalance = ERC20(staker).balanceOf(address(this));
-        if (stakerBalance != 0) {
-            IStaker(staker).unstake(stakerBalance, address(this));
-        }
-        ERC20(WAR).approve(staker, 0);
-
-        ERC20(WAR).approve(newStaker, type(uint256).max);
-        uint256 warBalance = asset.balanceOf(address(this));
-        if (warBalance != 0) {
-            IStaker(newStaker).stake(warBalance, address(this));
-        }
-        staker = newStaker;
-    }
-
-    function afterDeposit(uint256 assets, uint256 /* shares */ ) internal override {
-        IStaker(staker).stake(assets, address(this));
-    }
-
-    function beforeWithdraw(uint256 assets, uint256 /*shares */ ) internal override {
-        IStaker(staker).unstake(assets, address(this));
-    }
-
+    /**
+     * @dev totalAssets is the total number of stkWAR
+     */
     function totalAssets() public view override returns (uint256) {
         uint256 assets = ERC20(staker).balanceOf(address(this));
 
         return assets;
+    }
+
+    /**
+     * @notice update the swapper contract to a new one
+     * @param newSwap the new swapper contract
+     * @custom:requires owner
+     */
+    function setSwap(address newSwap) external onlyOwner {
+        swap = newSwap;
+    }
+
+    /**
+     * @notice update the staker contract to a new one
+     * @param newStaker the new staker contract
+     * @custom:requires owner
+     */
+    function setStaker(address newStaker) external onlyOwner {
+        // Unstake all wars from old staker
+        uint256 stakerBalance = ERC20(staker).balanceOf(address(this));
+        if (stakerBalance != 0) {
+            IStaker(staker).unstake(stakerBalance, address(this));
+        }
+        // revoke allowance from old staker
+        ERC20(WAR).approve(staker, 0);
+
+        // approve all war tokens to be spent by new staker
+        ERC20(WAR).approve(newStaker, type(uint256).max);
+
+        // Restake all tokens
+        uint256 warBalance = asset.balanceOf(address(this));
+        if (warBalance != 0) {
+            IStaker(newStaker).stake(warBalance, address(this));
+        }
+
+        staker = newStaker;
+    }
+
+    /**
+     * @dev stake assets after each deposit
+     */
+    function afterDeposit(uint256 assets, uint256 /* shares */ ) internal override {
+        IStaker(staker).stake(assets, address(this));
+    }
+
+    /**
+     * @dev unstake assets before each withdraw to have enough WAR to transfer
+     */
+    function beforeWithdraw(uint256 assets, uint256 /*shares */ ) internal override {
+        IStaker(staker).unstake(assets, address(this));
     }
 }
