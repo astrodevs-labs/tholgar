e52edb2d4b86735ba9839e6d15c967f26f292672
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-06-17 16:24:20 +0200

    fix: Owner now has good import

--- lib/warlord/src/utils/Owner.sol
+++ lib/warlord/src/utils/Owner.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import "openzeppelin/access/Ownable.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
 /**
  * @title Extend OZ Ownable contract
