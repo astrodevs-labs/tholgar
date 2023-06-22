6c14e80a0de3b3766010d991e374a777d13d082a
0xMemoryGrinder <alexjublot@gmail.com>
2023-06-22 23:36:59 +0200

    fix: remove bad merge conflict

--- contracts/test/utils/constants.sol
+++ contracts/test/utils/constants.sol
@@ -6,9 +6,5 @@ address constant WAR = 0xa8258deE2a677874a48F5320670A869D74f0cbC1;
 address constant STAKER = 0xA86c53AF3aadF20bE5d7a8136ACfdbC4B074758A;
 address constant MINTER = 0x144a689A8261F1863c89954930ecae46Bd950341;
 
-<<<<<<< 0f12f360120b9b309cbb93e0448012dd08dc8dbd:contracts/src/utils/constants.sol
-uint256 constant MAX_WEIGHT = 1e5;
-=======
 address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 address constant AUGUSTUS_SWAPPER = 0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57;
->>>>>>> refactor: use constants only in test:contracts/test/utils/constants.sol
