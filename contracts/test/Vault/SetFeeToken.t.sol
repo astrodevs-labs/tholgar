fb07147eb0ec91d567253228a59303c240600add
0xMemoryGrinder <alexjublot@gmail.com>
2023-06-26 22:46:00 +0200

    test(contracts): added Vault setFeeToken tests

---
+++ contracts/test/Vault/SetFeeToken.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.20;
+
+import "./VaultTest.sol";
+import {Errors} from "../../src/utils/Errors.sol";
+
+contract SetFeeToken is VaultTest {
+    function setUp() public override {
+        VaultTest.setUp();
+    }
+
+    function test_setFeeToken_normal() public {
+        vm.prank(owner);
+        vault.setFeeToken(address(usdc));
+        assertEq(address(vault.feeToken()), address(usdc));
+    }
+
+    function test_setFeeToken_NotOwner() public {
+        vm.prank(bob);
+        vm.expectRevert("Ownable: caller is not the owner");
+        vault.setFeeToken(address(usdc));
+    }
+
+    function test_setFeeToken_ZeroAddress() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        vm.prank(owner);
+        vault.setFeeToken(address(0));
+    }
+}
