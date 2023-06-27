3949da9827b47df7409ebe40df3dc2b898b4f7da
0xMemoryGrinder <alexjublot@gmail.com>
2023-06-27 22:51:12 +0200

    test(contracts): added Vault pause tests

---
+++ contracts/test/Vault/Pause.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.20;
+
+import "./VaultTest.sol";
+import {Errors} from "../../src/utils/Errors.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract Pause is VaultTest {
+    function setUp() public override {
+        VaultTest.setUp();
+    }
+
+    function test_pause_normal() public {
+        vm.prank(owner);
+        vault.pause();
+        assertTrue(vault.paused());
+    }
+
+    function test_pause_NotOwner() public {
+        vm.prank(bob);
+        vm.expectRevert("Ownable: caller is not the owner");
+        vault.pause();
+    }
+}
