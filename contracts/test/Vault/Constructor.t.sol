d7f44e870a33a23f557e6ebfa4411602c1cb88ae
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-07-04 18:20:56 +0200

    tests: Vault constructor tests

--- contracts/test/Vault/Constructor.t.sol
+++ contracts/test/Vault/Constructor.t.sol
@@ -1,45 +1,34 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.20;
 
-import "../MainnetTest.sol";
-import {ASwapper} from "../../src/abstracts/ASwapper.sol";
-import {WarStaker} from "warlord/Staker.sol";
-import {Vault} from "../../src/Vault.sol";
-import {Errors} from "../../src/utils/Errors.sol";
+import "./VaultTest.sol";
 
-contract Constructor is MainnetTest {
-    Vault vault;
+contract Constructor is VaultTest {
 
-    function setUp() public virtual override {
-        MainnetTest.setUp();
-        fork();
-    }
-
-    function test_VaultContructor_GoodDeployment() public {
-        vm.startPrank(owner);
-        ASwapper.OutputToken[] memory tokens = new ASwapper.OutputToken[](1);
-        tokens[0] = ASwapper.OutputToken(address(usdc), 10_000);
-        WarStaker staker = new WarStaker(address(war));
-        vault =
-        new Vault(address(staker), address(minter), 500, owner, address(usdc), augustusSwapper, tokenTransferAddress, operator, address(war));
-        vault.setOutputTokens(tokens);
-        vm.stopPrank();
-
+    function test_constructor_Normal() public {
+        assertEq(vault.owner(), owner, "Owner is not owner");
         assertFalse(vault.paused(), "Vault is paused");
+        assertEq(vault.minter(), address(minter), "Minter is not minter");
         assertEq(address(vault.asset()), address(war), "Asset is not WAR");
         assertEq(vault.staker(), address(staker), "Staker is not staker");
         assertEq(vault.asset().allowance(address(vault), address(staker)), UINT256_MAX, "Staker allowance is not max");
     }
 
-    /*
-    this is fine
-    function test_VaultConstructor_zeroStakerAddress() public {
-        ASwapper.OutputToken[] memory tokens = new ASwapper.OutputToken[](1);
-        tokens[0] = ASwapper.OutputToken(address(usdc), 18, 10_000);
-        //vm.expectRevert(Errors.ZeroAddress.selector);
+    function test_constructor_ZeroAddressAsset() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
         vm.prank(owner);
-        vault = new Vault(address(0), minter, 500, owner, address(usdc), augustusSwapper, operator, address(war));
-        vault.setOutputTokens(tokens);
+        vault = new Vault(address(staker), address(minter), 500, owner, address(usdc), augustusSwapper, tokenTransferAddress, operator, address(0));
     }
-    */
+
+    function test_constructor_ZeroAddressStaker() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        vm.prank(owner);
+        vault = new Vault(address(0), address(minter), 500, owner, address(usdc), augustusSwapper, tokenTransferAddress, operator, address(war));
+    }
+
+    function test_constructor_ZeroAddressMinter() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        vm.prank(owner);
+        vault = new Vault(address(staker), address(0), 500, owner, address(usdc), augustusSwapper, tokenTransferAddress, operator, address(war));
+    }
 }
