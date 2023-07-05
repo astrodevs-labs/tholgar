7504823a25e20dd31a99afae599d44200f380943
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-07-05 23:21:54 +0200

    feat(frontend): reworked Container component to add BoxProps to available props

--- frontend/src/components/ui/Container/Container.tsx
+++ frontend/src/components/ui/Container/Container.tsx
@@ -1,23 +1,21 @@
 import React, {FC, PropsWithChildren} from "react";
-import {Box, useColorMode, useColorModeValue} from "@chakra-ui/react";
+import {Box, BoxProps, useColorModeValue} from "@chakra-ui/react";
 import BorderedComponentProps, {defaultBorderedComponentProps} from "utils/BorderedComponentProps";
 import useFormatBorderProps from "hooks/useFormatBorderProps";
 
-export interface ContainerProps extends BorderedComponentProps{
+export interface ContainerProps extends BorderedComponentProps, BoxProps{
   lightColor?: string;
   darkColor?: string;
 }
 
 export const Container: FC<PropsWithChildren<ContainerProps>> = (props) => {
-  const {lightColor, darkColor} = props;
-  const {colorMode} = useColorMode();
+  const {lightColor, darkColor, ...rest} = props;
   const backgroundColor = useColorModeValue(lightColor, darkColor);
-
   const borderProps = useFormatBorderProps(props);
-  console.log(borderProps);
+  const {children, borders, bordersRounded, borderType, borderThickness, bordersRadius, borderslightColor, bordersdarkColor, ...boxProps} = rest;
 
   return (
-    <Box backgroundColor={backgroundColor} {...borderProps} >
+    <Box backgroundColor={backgroundColor} {...borderProps} {...boxProps} >
       {props.children}
     </Box>
   )
