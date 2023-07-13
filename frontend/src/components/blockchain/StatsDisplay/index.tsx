52fbd938dae71b555dc66cefaddd77fe5ef35fe7
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-07-13 23:48:17 +0200

    feat: use only once getBalance of staker

--- frontend/src/components/blockchain/StatsDisplay/index.tsx
+++ frontend/src/components/blockchain/StatsDisplay/index.tsx
@@ -17,118 +17,123 @@ import getTotalPricePerToken from 'utils/getTotalPricePerToken';
 
 export interface StatsDisplayProps {}
 
-const CirculatingSupply: FC = () => {
-  const { data, isLoading, isError } = useToken({
-    address: vaultAddress
-  });
-
-  return (
-    <VStack>
-      <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
-        {'Circulating Supply'}
-      </Text>
-      <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
-        {isLoading ? '0' : isError ? '0' : formatNumber(data!.totalSupply.formatted)}
-      </Text>
-    </VStack>
-  );
-};
-
-const APY: FC = () => {
-  const { data, isLoading, isError } = useBalance({
-    address: vaultAddress,
-    token: stakerAddress
-  });
-
-  return (
-    <VStack>
-      <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
-        {'APY'}
-      </Text>
-      <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
-        {isLoading ? '0%' : isError ? '0%' : formatNumber(data!.formatted)}
-      </Text>
-    </VStack>
-  );
-};
-
-const WARLocked: FC = () => {
-  const { data, isLoading, isError } = useBalance({
-    address: vaultAddress,
-    token: stakerAddress
-  });
-
-  return (
-    <VStack>
-      <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
-        {'WAR Locked'}
-      </Text>
-      <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
-        {isLoading ? '0' : isError ? '0' : formatNumber(data!.formatted)}
-      </Text>
-    </VStack>
-  );
-};
-
-const TVL: FC = () => {
-  const [tvl, setTvl] = React.useState<string>('0$');
-
-  const { data: warBalance } = useBalance({
-    address: '0xb95A4779CceDc53010EF0df8Bf8Ed6aEB0E8c2B2',
-    token: stakerAddress
-  });
-  const { data: weights } = useContractRead({
-    address: redeemerAddress,
-    abi: redeemerABI,
-    functionName: 'getTokenWeights'
-  });
-  const { data: auraRatio } = useContractRead({
-    address: ratioAddress,
-    abi: ratioABI,
-    functionName: 'getTokenRatio',
-    args: [auraAddress]
-  });
-  const { data: cvxRatio } = useContractRead({
-    address: ratioAddress,
-    abi: ratioABI,
-    functionName: 'getTokenRatio',
-    args: [cvxAddress]
-  });
-
-  async function computeTVL() {
-    if (warBalance === undefined || weights === undefined || auraRatio === undefined || cvxRatio === undefined) return;
-    const auraWeight = (weights as any).find((weight: any) => weight.token === auraAddress);
-    const cvxWeight = (weights as any).find((weight: any) => weight.token === cvxAddress);
-
-    const auraPrice = await getTotalPricePerToken(
-      (warBalance.value * auraWeight.weight) / (auraRatio as bigint) / BigInt(1e18),
-      auraAddress
-    );
-    const cvxPrice = await getTotalPricePerToken(
-      (warBalance.value * cvxWeight.weight) / (cvxRatio as bigint) / BigInt(1e18),
-      cvxAddress
-    );
-    const total = auraPrice + cvxPrice;
-    setTvl(formatNumber(total.toFixed(0).toString()) + '$');
-  }
-
-  useEffect(() => {
-    computeTVL();
-  }, [warBalance, weights, auraRatio, cvxRatio]);
-
-  return (
-    <VStack>
-      <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
-        {'Total Volume Locked'}
-      </Text>
-      <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
-        {tvl}
-      </Text>
-    </VStack>
-  );
-};
-
 export const StatsDisplay: FC<StatsDisplayProps> = () => {
+  const {
+    data: warBalance,
+    isLoading: isWarBalanceLoading,
+    isError: isWarBalanceError
+  } = useBalance({
+    address: vaultAddress,
+    token: stakerAddress
+  });
+
+  const CirculatingSupply: FC = () => {
+    const { data, isLoading, isError } = useToken({
+      address: vaultAddress
+    });
+
+    return (
+      <VStack>
+        <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
+          {'Circulating Supply'}
+        </Text>
+        <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
+          {isLoading ? '0' : isError ? '0' : formatNumber(data!.totalSupply.formatted)}
+        </Text>
+      </VStack>
+    );
+  };
+
+  const APY: FC = () => {
+    return (
+      <VStack>
+        <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
+          {'APY'}
+        </Text>
+        <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
+          {'??%'}
+        </Text>
+      </VStack>
+    );
+  };
+
+  const WARLocked: FC = () => {
+    return (
+      <VStack>
+        <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
+          {'WAR Locked'}
+        </Text>
+        <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
+          {isWarBalanceLoading
+            ? '0'
+            : isWarBalanceError
+            ? '0'
+            : formatNumber(warBalance!.formatted)}
+        </Text>
+      </VStack>
+    );
+  };
+
+  const TVL: FC = () => {
+    const [tvl, setTvl] = React.useState<string>('0$');
+
+    const { data: weights } = useContractRead({
+      address: redeemerAddress,
+      abi: redeemerABI,
+      functionName: 'getTokenWeights'
+    });
+    const { data: auraRatio } = useContractRead({
+      address: ratioAddress,
+      abi: ratioABI,
+      functionName: 'getTokenRatio',
+      args: [auraAddress]
+    });
+    const { data: cvxRatio } = useContractRead({
+      address: ratioAddress,
+      abi: ratioABI,
+      functionName: 'getTokenRatio',
+      args: [cvxAddress]
+    });
+
+    async function computeTVL() {
+      if (
+        warBalance === undefined ||
+        weights === undefined ||
+        auraRatio === undefined ||
+        cvxRatio === undefined
+      )
+        return;
+      const auraWeight = (weights as any).find((weight: any) => weight.token === auraAddress);
+      const cvxWeight = (weights as any).find((weight: any) => weight.token === cvxAddress);
+
+      const auraPrice = await getTotalPricePerToken(
+        (warBalance.value * auraWeight.weight) / (auraRatio as bigint) / BigInt(1e18),
+        auraAddress
+      );
+      const cvxPrice = await getTotalPricePerToken(
+        (warBalance.value * cvxWeight.weight) / (cvxRatio as bigint) / BigInt(1e18),
+        cvxAddress
+      );
+      const total = auraPrice + cvxPrice;
+      setTvl(formatNumber(total.toFixed(0).toString()) + '$');
+    }
+
+    useEffect(() => {
+      computeTVL();
+    }, [warBalance, weights, auraRatio, cvxRatio]);
+
+    return (
+      <VStack>
+        <Text whiteSpace={'nowrap'} fontSize={'1.125em'} color={'gray.500'}>
+          {'Total Volume Locked'}
+        </Text>
+        <Text whiteSpace={'nowrap'} fontSize={'1.5em'} fontWeight={'bold'}>
+          {tvl}
+        </Text>
+      </VStack>
+    );
+  };
+
   return (
     <Container>
       <Grid display={'flex'} gap={'2em'} templateColumns={'repeat(3, 1fr)'}>
