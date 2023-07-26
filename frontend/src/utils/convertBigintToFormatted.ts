6b04702c3f1d310b2950052cd6acd0a975659636
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-07-26 18:15:13 +0200

    fix: convertBigIntToFormatted now works as intended

--- frontend/src/utils/convertBigintToFormatted.ts
+++ frontend/src/utils/convertBigintToFormatted.ts
@@ -5,7 +5,19 @@ export default function convertBigintToFormatted(
   const numberAsString = number.toString();
   const nbDecimals = numberAsString.length - decimals;
   const numberWithoutDecimals = numberAsString.slice(0, nbDecimals);
-  const numberWithDecimals = numberWithoutDecimals + '.' + numberAsString.slice(nbDecimals);
+  let numberWithDecimals = numberWithoutDecimals + '.' + numberAsString.slice(nbDecimals);
+
+  for (let i = 0; i < decimals; i++) {
+    if (numberWithDecimals[numberWithDecimals.length - 1] === '0') {
+      numberWithDecimals = numberWithDecimals.slice(0, -1);
+    } else {
+      break;
+    }
+  }
+
+  if (numberWithDecimals[numberWithDecimals.length - 1] === '.') {
+    numberWithDecimals = numberWithDecimals.slice(0, -1);
+  }
 
   return numberWithDecimals;
 }
