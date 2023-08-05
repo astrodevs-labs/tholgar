27013acbb9852396b22a4090a25954554187af7a
EnergyCube <vincent.mardirossian@epitech.eu>
2023-08-05 05:56:34 +0200

    feat: reworked faq page with Accordion

--- frontend/src/pages/FAQ/index.tsx
+++ frontend/src/pages/FAQ/index.tsx
@@ -1,19 +1,57 @@
-import { Divider, Heading } from '@chakra-ui/react';
+import { Heading, useColorModeValue } from '@chakra-ui/react';
 import { FC } from 'react';
-import { QuestionDisplay } from 'components/ui/QuestionDisplay';
 import { questions } from '../../config/questions';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Card,
+  CardHeader,
+  CardBody,
+  Divider
+} from '@chakra-ui/react';
 
 const FAQ: FC = () => (
   <>
-    <Heading size={'lg'} marginBottom={3}>
-      Frequently Asked Questions
-    </Heading>
-    {questions.map((question, index) => (
-      <>
-        <QuestionDisplay key={index} question={question.question} answer={question.answer} />
-        {index !== questions.length - 1 && <Divider />}
-      </>
-    ))}
+    <Card
+      backgroundColor={useColorModeValue('background.500.light', 'background.500.dark')}
+      borderRadius="lg"
+      borderColor={useColorModeValue('border.light', 'border.dark')}
+      margin={'2rem'}
+      borderWidth="1px">
+      <CardHeader>
+        <Heading as="h1" size="lg">
+          Frequently Asked Questions
+        </Heading>
+      </CardHeader>
+      <Divider borderColor={useColorModeValue('border.light', 'border.dark')} />
+      <CardBody>
+        <Accordion allowToggle>
+          {questions.map((question, index) => (
+            <AccordionItem key={index} border="none">
+              <AccordionButton>
+                <Heading as="h2" size="ms" flex="1" textAlign="left">
+                  {question.question}
+                </Heading>
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel pb={2} marginLeft={4}>
+                {question.answer instanceof Function ? question.answer() : question.answer}
+              </AccordionPanel>
+              {index < questions.length - 1 && (
+                <Divider
+                  margin={'1rem 0'}
+                  borderColor={useColorModeValue('border.light', 'border.dark')}
+                />
+              )}
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </CardBody>
+    </Card>
   </>
 );
 
