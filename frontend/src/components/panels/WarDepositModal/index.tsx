f0d97c285a7dc5b48be9676b3e3c5eba8b4f54d0
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-08-21 17:16:07 +0200

    refactor: use ApproveAllowance component instead of custom step

--- frontend/src/components/panels/WarDepositModal/index.tsx
+++ frontend/src/components/panels/WarDepositModal/index.tsx
@@ -1,24 +1,18 @@
 import { FC, useCallback, useEffect } from 'react';
 import {
   Button,
-  Center,
   Flex,
-  HStack,
   Spinner,
-  Switch,
-  Text,
-  useBoolean,
   useColorModeValue
 } from '@chakra-ui/react';
 import {
-  erc20ABI,
   useAccount,
   useContractWrite,
-  useContractRead,
   useWaitForTransaction
 } from 'wagmi';
-import { maxAllowance, vaultABI, vaultAddress, warAddress } from '../../../config/blockchain';
+import {vaultABI, vaultAddress, warAddress} from '../../../config/blockchain';
 import { useStore } from '../../../store';
+import {ApproveAllowance} from "../../blockchain/ApproveAllowance/Index";
 
 export interface WarDepositModalProps {
   step: number;
@@ -29,70 +23,6 @@ interface StepProps {
   validateStep: () => void;
   address: `0x${string}`;
 }
-
-const Step1: FC<StepProps> = ({ validateStep, address }) => {
-  const warDepositInputAmount = useStore((state) => state.getDepositInputTokenAmount('war'));
-  const [allowTotal, setAllowTotal] = useBoolean(false);
-  const [validated, setValidated] = useBoolean(false);
-  const { data, write } = useContractWrite({
-    address: warAddress,
-    abi: erc20ABI,
-    functionName: 'approve'
-  });
-  const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash
-  });
-  const { data: allowanceResData } = useContractRead({
-    address: warAddress,
-    abi: erc20ABI,
-    functionName: 'allowance',
-    args: [address, vaultAddress]
-  });
-  const allow = useCallback(() => {
-    if (!isLoading && !isSuccess) {
-      write({
-        args: [vaultAddress, allowTotal ? maxAllowance : warDepositInputAmount]
-      });
-    }
-  }, [warDepositInputAmount, allowTotal]);
-
-  useEffect(() => {
-    if (isSuccess) {
-      validateStep();
-    }
-  }, [isSuccess]);
-
-  useEffect(() => {
-    if (allowanceResData && allowanceResData >= warDepositInputAmount && !validated) {
-      setValidated.on();
-      validateStep();
-    }
-  }, [allowanceResData, validated]);
-
-  return (
-    <Flex direction={'column'}>
-      <HStack>
-        <Text>Allowance type : </Text>
-        <Center>
-          <HStack>
-            <Text>Deposit amount</Text>
-            <Switch onChange={setAllowTotal.toggle} colorScheme="green" />
-            <Text>Max allowance</Text>
-          </HStack>
-        </Center>
-      </HStack>
-      <Button
-        my={5}
-        onClick={allow}
-        disabled={isLoading}
-        backgroundColor={useColorModeValue('brand.primary.200', 'brand.primary.300')}
-        _hover={{ bgColor: useColorModeValue('brand.primary.300', 'brand.primary.100') }}
-        color={useColorModeValue('#00cf6f', 'inherit')}>
-        {isLoading ? <Spinner /> : 'Approve'}
-      </Button>
-    </Flex>
-  );
-};
 
 const Step2: FC<StepProps> = ({ validateStep, address }) => {
   const warDepositInputAmount = useStore((state) => state.getDepositInputTokenAmount('war'));
@@ -111,7 +41,7 @@ const Step2: FC<StepProps> = ({ validateStep, address }) => {
         args: [warDepositInputAmount, address]
       });
     }
-  }, [warDepositInputAmount, validateStep]);
+  }, [isLoading, isSuccess, write, warDepositInputAmount, address]);
 
   useEffect(() => {
     if (isSuccess) {
@@ -138,7 +68,15 @@ export const WarDepositModal: FC<WarDepositModalProps> = ({ step, validateStep }
   const { address } = useAccount();
 
   if (step == 0) {
-    return <Step1 validateStep={validateStep} address={address!} />;
+    return (
+    <ApproveAllowance
+      token={'war'}
+      tokenAddress={warAddress}
+      step={step}
+      validateStep={validateStep}
+      address={address!}
+    />
+  );
   } else if (step == 1) {
     return <Step2 validateStep={validateStep} address={address!} />;
   }
